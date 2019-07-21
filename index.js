b95d0a3b2253fbbe0edeb6e138181094028c68e3
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 02:31:13 +0000

    advanced AJAx

---
+++ index.js
@@ -0,0 +1,32 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
